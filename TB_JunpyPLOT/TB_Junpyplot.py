1070c1867de77cd20166e8d878d4897666d6759d
tangtang <a5511572@gmail.com>
2025-04-15 15:01:34 +0800

    Adding MutiR function

--- TB_JunpyPLOT/TB_Junpyplot.py
+++ TB_JunpyPLOT/TB_Junpyplot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 from STT_Tool import Tools
 color = Tools.ColorList()
@@ -81,10 +82,11 @@ class SingleR_TB_Plot():
                        color=color[-1]
                        )
             x_xpos = xpos-0.5
-        
+    
+        xlabel = [f"R{i}" for i in range(Rtot, 0, -1)]
         plt.xlim(-0.5, xpos-0.5)
         plt.xticks(ticks=xpos_list,
-                   labels=[f"R{i}" for i in range(Rtot, 0, -1)]
+                   labels=xlabel
                    )
         plt.ylim(
             (minimum-0.5*scale)/scale, (maximum+0.5*scale)/scale
@@ -97,8 +99,13 @@ class SingleR_TB_Plot():
         else:
             plt.show()
         # =====
+        LABELoutput  = (xlabel, xpos_list)
+        RESULToutput = data_caseList
+        # =====
         self.rescale     = rescale
         self.maxminBound = maxminBound
+        # =====
+        return LABELoutput, RESULToutput
 
     def RPlot(self, p=False):
         
@@ -141,9 +148,10 @@ class SingleR_TB_Plot():
             (minimum-0.5*10**(6))/scale, (maximum+0.5*10**(6))/scale
             )
 
+        xlabel = [f"R{i}" for i in range(Rtot, 0, -1)]
         plt.xlim(-0.5, xpos-0.5)
         plt.xticks(ticks=xpos_list,
-                labels=[f"R{i}" for i in range(Rtot, 0, -1)])
+                labels=xlabel)
 
         plt.ylabel("Resistance (MΩ)")
 
@@ -154,8 +162,139 @@ class SingleR_TB_Plot():
             plt.savefig("Rcase_Result.png")
         else:
             plt.show()
+        # =====
+        LABELoutput  = (xlabel, xpos_list)
+        RESULToutput = data_RList
+        # =====
 
 class MultiR_TB_Plot():
-    def __init__(self, DataPath):
-        DataPath_List = []
+    def __init__(self, Rtot, DataPath="Temperature_Rparse"):
+        
+        path = os.listdir(f"{DataPath}")
+
+        targetPath = []
+        for p in path:
+            if p[-4:] == ".dat":
+                targetPath.append(p)
+        
+        self.targetPath = targetPath
+        self.Rtot       = Rtot
+    
+    def Tool_BoundarySet(self, data, Bound:tuple):
+        maximum = Bound[0]
+        minimum = Bound[1]
+        
+        if np.max(data)>maximum:
+            maximum = np.max(data)
+        else:
+            pass
+        if np.min(data)<minimum:
+            minimum = np.min(data)
+        else:
+            pass
+
+        return maximum, minimum    
+        
+
+    def Tool_LoadData(self, DataPath):
+        
+        Rtot = self.Rtot
+
+        with open(f"{DataPath}") as R_File:
+            data = R_File.readlines()
+
+        data_caseList = [[] for _ in range(Rtot)]
+        data_RList = [[] for _ in range(Rtot)]
+        for case in data:
+            case = case.split()
+            del case[1]
+            Rgroup = int(case[0][1:])-1
+            
+            for i in case[1:]:
+                data_RList[Rgroup].append(float(i))
+
+            data_caseList[Rgroup].append(np.float64(case[1:]))
+        
+        return data_caseList, data_RList
+
+    def Tool_casePlot(self, ax, case_data, color, scale=1e6, maxminBound=(-1e10, 1e10)):
+        
+        data_caseList = case_data
+        maximum = maxminBound[0]
+        minimum = maxminBound[1]
+
+        xpos = 0
+        x_xpos = -0.5
+        xpos_list = []
+        for case in data_caseList[::-1]:
+            for subcase in case:
+                x = [xpos for _ in range(len(subcase))]
+                # ax.scatter(x, subcase/scale, marker='*',
+                #             c='k',
+                #             alpha=0.5
+                #             )
+                ax.errorbar(xpos, np.mean(subcase)/scale,
+                            fmt='o', color=color[0],
+                            yerr=np.std(subcase)/scale,
+                            ecolor=color[1],
+                            capsize=4, elinewidth=2,
+                            # label=label
+                            )
+                xpos = xpos+1
+
+                maximum, minimum = self.Tool_BoundarySet(data=subcase, Bound=(maximum, minimum))
+            
+            xpos_list.append((x_xpos+(xpos-0.5))/2)
+            ax.vlines(x=xpos-0.5, 
+                       ymin=(minimum-scale*10)/scale, ymax=(maximum+scale*10)/scale, 
+                       color='k', alpha=0.5
+                       )
+            x_xpos = xpos-0.5
+
+        return maximum, minimum, xpos, xpos_list
+
+    def casePlot(self, rescale=1e6, maxminBound=(-1e10, 1e10), p=False):
+        maximum = maxminBound[0]
+        minimum = maxminBound[1]
+        scale   = rescale
+
+        Rtot          = self.Rtot
+        targetPath    = self.targetPath
+        
+        # ==========
+
+        plt.figure(figsize=(8, 4))
+        plt.rcParams['ytick.direction']='in'
+        ax = plt.subplot(111)
+
+        for i, datapath in enumerate(targetPath):
+            data_caseList, data_RList = self.Tool_LoadData(DataPath=f"Temperature_Rparse/{datapath}")
+            maximum, minimum, xpos, xpos_list = self.Tool_casePlot(ax, data_caseList, 
+                                                                   color=(color[i], color[i]), 
+                                                                #    label=f"{targetPath[:5]}",
+                                                                   scale=1e6, 
+                                                                   maxminBound=(maximum, minimum)
+                                                                   )
+    
+        xlabel = [f"R{i}" for i in range(Rtot, 0, -1)]
+        plt.xlim(-0.5, xpos-0.5)
+        plt.xticks(ticks  = xpos_list,
+                   labels = xlabel
+                   )
+        plt.ylim(
+            (minimum-0.5*scale)/scale, (maximum+0.5*scale)/scale
+            )
+        plt.ylabel("Resistance (MΩ)")
+        plt.grid("--", axis='y')
+        # plt.legend(loc='best')
+
+        if p:
+            plt.savefig("Rcase_Result.png")
+        else:
+            plt.show()
+
+
+
+        
+        
     
